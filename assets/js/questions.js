47b8e8fd62839b37df6a206d2d1e567fbcbc07ad
Marcin Tatarski <martin.tatarski>
2023-12-01 19:43:18 +0000

    Add quiz questions

---
+++ assets/js/questions.js
@@ -0,0 +1,17 @@
+const questions = [
+  {
+    'The result of adding a number and a string in JavaScript is' : ['A new data type', 'A mathematical error', 'A string concatenation', 'A boolean value']
+  },
+  {
+    'The typeof null in JavaScript returns' : ['object', 'null', 'undefined', 'string']
+  },
+  {
+    'Array in JavaScript is used to store': ['Numbers', 'Multiple values', 'Colors', 'Time']
+  },
+  {
+    'The === operator in JavaScript is used for': ['Checking equality with type coercion', 'Concatenating strings', 'Checking equality without type coercion', 'Assigning values to variables']
+  },
+  {
+    '`setTimeout` function in JavaScript is primarily used for': ['Pausing script execution', 'Setting timers to execute functions after a delay', 'Immediate function execution', 'Changing CSS styles']
+  },
+];
